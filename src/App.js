8d94084db7097bb1787d60777ec6826d00767df3
Dee Race <deerace12@gmail.com>
2019-01-29 15:43:38 -0500

    add font awesome, edit app.js, bootstrap hooked up, remove previous css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>Shop Succulents</p>
-        </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Column One</div>
+          <div className="col-6">
+            <span>
+              <i className="fas fa-home" />
+            </span>
+          </div>
+        </div>
       </div>
     );
   }
