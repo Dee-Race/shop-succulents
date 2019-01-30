11bf590bd798453f4d0de6b587378d8adece5cd9
Dee Race <deerace12@gmail.com>
2019-01-30 17:07:22 -0500

    add bootstrap and inital content in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import ProductList from "./ProductList";
 
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello Navbar</h3>
-      </div>
+      <nav className="navbar navbar-expand-sm bg-primary navbar-light px-sm-5">
+        <Link to="/" className="navbar-brand">
+          <h3>Home</h3>
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              Succulents
+            </Link>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
