12ce0f3c419c1642fe2453b146d0ec6f8055e650
Dee Race <deerace12@gmail.com>
2019-02-02 09:29:33 -0500

    start adding succulent product data array info

--- src/data.js
+++ src/data.js
@@ -3,3 +3,18 @@
 
 // use unsplash images for display in product list
 // https://unsplash.com/search/photos/succulents
+
+data = {
+  succulent1: [],
+  succulent2: [],
+  succulent3: [],
+  succulent4: [],
+  succulent4: [],
+
+  succulent5: [],
+  succulent5: [],
+  succulent6: [],
+  succulent7: [],
+
+  succulent8: []
+};
