40b436cd17160d3a88c8bfebefd7231b58c85e95
Dee Race <deerace12@gmail.com>
2019-02-08 15:33:20 -0500

    Update data.js with id, title, img, price, and description on product data

--- src/data.js
+++ src/data.js
@@ -4,17 +4,75 @@
 // use unsplash images for display in product list
 // https://unsplash.com/search/photos/succulents
 
-data = {
-  succulent1: [],
-  succulent2: [],
-  succulent3: [],
-  succulent4: [],
-  succulent4: [],
-
-  succulent5: [],
-  succulent5: [],
-  succulent6: [],
-  succulent7: [],
-
-  succulent8: []
-};
+export const productData = [
+  {
+    id: 1,
+    title: "Succulent 1",
+    img: "",
+    price: 15,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 2,
+    title: "Succulent 2",
+    img: "",
+    price: 10,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 3,
+    title: "Succulent 3",
+    img: "",
+    price: 25,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 4,
+    title: "Succulent 4",
+    img: "",
+    price: 15,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 5,
+    title: "Succulent 5",
+    img: "",
+    price: 18,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 6,
+    title: "Succulent 6",
+    img: "",
+    price: 30,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 7,
+    title: "Succulent 7",
+    img: "",
+    price: 15,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 8,
+    title: "Succulent 8",
+    img: "",
+    price: 15,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 9,
+    title: "Succulent 9",
+    img: "",
+    price: 10,
+    description: "Lorem Ipsum"
+  },
+  {
+    id: 10,
+    title: "Succulent 10",
+    img: "",
+    price: 25,
+    description: "Lorem Ipsum"
+  }
+];
