a0a282e3482ceaaf82012f509cf3f3aa43f30db6
Dee Race <deerace12@gmail.com>
2019-02-12 15:39:22 -0500

    add count and total to data product list objects, add more Lorem Ipsum for description

--- src/data.js
+++ src/data.js
@@ -10,70 +10,100 @@ export const productData = [
     title: "Succulent 1",
     img: "",
     price: 15,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 2,
     title: "Succulent 2",
     img: "",
     price: 10,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 3,
     title: "Succulent 3",
     img: "",
     price: 25,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 4,
     title: "Succulent 4",
     img: "",
     price: 15,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 5,
     title: "Succulent 5",
     img: "",
     price: 18,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 6,
     title: "Succulent 6",
     img: "",
     price: 30,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 7,
     title: "Succulent 7",
     img: "",
     price: 15,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 8,
     title: "Succulent 8",
     img: "",
     price: 15,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 9,
     title: "Succulent 9",
     img: "",
     price: 10,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   },
   {
     id: 10,
     title: "Succulent 10",
     img: "",
     price: 25,
-    description: "Lorem Ipsum"
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    count: 0,
+    total: 0
   }
 ];
 
@@ -83,7 +113,8 @@ export const productDetail = [
     title: "Succulent 1",
     img: "",
     price: 15,
-    description: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     inCart: false,
     count: 0,
     total: 0
