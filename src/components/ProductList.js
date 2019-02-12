5e38e18ae1fa65490797828ad890ae4e0b483e29
Dee Race <deerace12@gmail.com>
2019-02-12 15:07:22 -0500

    add ProductConsumer to productList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { productData } from "../data";
+import { ProductConsumer } from "../context";
 
 class ProductList extends Component {
   state = {
@@ -15,12 +16,16 @@ class ProductList extends Component {
         <div className="py-5">
           <div className="container">
             <Title name="our" title="succulents" />
-            <div className="row" />
+            <div className="row">
+              <ProductConsumer>
+                {hello => {
+                  return <h1>{hello}</h1>;
+                }}
+              </ProductConsumer>
+            </div>
           </div>
         </div>
       </React.Fragment>
-
-      //<Product />
     );
   }
 }
