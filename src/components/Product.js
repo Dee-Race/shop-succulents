03f672a305d017578cae0253f8dfd9d5633eb3b4
Dee Race <deerace12@gmail.com>
2019-02-13 16:51:12 -0500

    fix div structure

--- src/components/Product.js
+++ src/components/Product.js
@@ -12,26 +12,27 @@ class Product extends Component {
           <div
             className="img-container p-5"
             onClick={() => console.log("Clicked")}
-          />
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() => {
+                console.log("added to cart");
+              }}
+            >
+              {inCart ? (
+                <p className="text-capitaliza mb-0" disabled>
+                  in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus" />
+              )}
+            </button>
+          </div>
         </div>
-        <Link to="/details">
-          <img src={img} alt="product" className="card-img-top" />
-        </Link>
-        <button
-          className="cart-btn"
-          disabled={inCart ? true : false}
-          onClick={() => {
-            console.log("added to cart");
-          }}
-        >
-          {inCart ? (
-            <p className="text-capitaliza mb-0" disabled>
-              in cart
-            </p>
-          ) : (
-            <i className="fas fa-cart-plus" />
-          )}
-        </button>
       </ProductWrapper>
     );
   }
