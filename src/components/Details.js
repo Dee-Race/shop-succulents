ca8812f8acc209a3f60c4bb6d20d8d847e9f3b38
Dee Race <deerace12@gmail.com>
2019-03-11 15:44:53 -0400

    add props to details.js

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,34 @@
 import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
 
 class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Succulent Deets!</h3>
-      </div>
+      <ProductConsumer>
+        {value => {
+          const {
+            id,
+            title,
+            img,
+            price,
+            description,
+            inCart
+          } = value.productDetail;
+          return (
+            <div className="container py-5">
+              {title}
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              {/* end title */}
+            </div>
+          );
+        }}
+      </ProductConsumer>
     );
   }
 }
